a92b443d53f748c39a0f15e697db46bb98ff72b6
Diana Kozlova <dianahaseo@gmail.com>
2026-01-27 22:34:38 +0300

    Homework 15.1

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,69 +1,75 @@
 import pytest
 from src.category import Category
 from src.product import Product
-from tests.conftest import json_categories
+from tests.conftest import json_categories, smartphones_category
+
 
 class TestCategory:
     @pytest.fixture(autouse=True)
     def reset_counters(self):
-        """Авто сброс счетчиков"""
+        """Автоматический сброс счетчиков перед каждым тестом"""
         Category.products_count = 0
         Category.category_count = 0
 
     def test_init_basic(self):
-        """✅ Задание 1: базовая init + приватность"""
+        """Базовая инициализация + счетчики"""
         product = Product("Test", "desc", 100.0, 1)
         cat = Category("TestCat", "Test desc", [product])
         assert cat.name == "TestCat"
         assert cat.description == "Test desc"
-        # Приватность: pytest.raises(AttributeError, getattr, cat, '__products')
+        assert "Test" in cat.products
         assert Category.products_count == 1
         assert Category.category_count == 1
 
-    def test_add_product(self):
-        """✅ Задание 1: add_product метод"""
+    def test_init_multiple_products(self):
+        """Несколько продуктов в init"""
+        p1 = Product("P1", "d1", 100, 1)
+        p2 = Product("P2", "d2", 200, 2)
+        cat = Category("Multi", "desc", [p1, p2])
+        assert Category.products_count == 2
+        assert "P1" in cat.products
+        assert "P2" in cat.products
+
+    def test_str(self):
         cat = Category("Test", "desc", [])
-        product = Product("New Product", "desc", 100.0, 1)
+        assert "Test, количество продуктов: 0 шт." in str(cat)
+
+    def test_class_counters_add_product(self):
+        """add_product + счетчик"""
+        cat = Category("Test", "desc", [])
+        product = Product("New", "desc", 100.0, 1)
         cat.add_product(product)
         assert Category.products_count == 1
-        assert "New Product" in cat.products
+        assert "New" in cat.products
+
+    def test_empty_products(self):
+        """Пустой список"""
+        cat = Category("Empty", "desc", [])
+        assert cat.products == ""
+        assert Category.products_count == 0
+
+    def test_none_products(self):
+        """products=None"""
+        cat = Category("None", "desc")
+        assert cat.products == ""
+        assert Category.products_count == 0
 
     def test_products_property(self):
-        """✅ Задание 2: @property products"""
+        """Несколько продуктов в property"""
         p1 = Product("Samsung", "256GB", 180000.0, 5)
         p2 = Product("iPhone", "512GB", 210000.0, 8)
         cat = Category("Phones", "desc", [p1, p2])
-        assert "Samsung, 180000 руб. Остаток: 5 шт." in cat.products
-        assert len(cat.products.split("\n")) == 2
+        products_str = cat.products
+        assert "Samsung, 180000 руб." in products_str
+        assert "iPhone, 210000 руб." in products_str
+        assert len(products_str.split("\n")) == 2
 
-    def test_products_empty(self):
-        """Пустые случаи для property"""
-        cat1 = Category("Empty", "desc", [])
-        cat2 = Category("None", "desc")
-        assert cat1.products == ""
-        assert cat2.products == ""
-
-    def test_counters_init(self):
-        """Счетчики при init"""
-        p1 = Product("P1", "d1", 100, 1)
-        cat1 = Category("Cat1", "desc", [p1])  # +1 продукт, +1 категория
-        cat2 = Category("Cat2", "desc", [])     # +0 продуктов, +1 категория
-        assert Category.products_count == 1
-        assert Category.category_count == 2
-
-    def test_counters_add(self):
-        """Счетчики при add_product"""
-        cat = Category("Test", "desc", [])
-        p1 = Product("Add", "d1", 100, 1)
-        cat.add_product(p1)
-        assert Category.products_count == 1
-
-    def test_json_fixture(self, json_categories):
-        """Тест conftest"""
+    def test_json_categories(self, json_categories):
+        """JSON fixture"""
         assert len(json_categories) == 2
         assert json_categories[0].name == "Смартфоны"
 
-    def test_real_fixture(self, smartphones_category):
-        """Реальные данные"""
+    def test_real_data(self, smartphones_category):
+        """Реальная фикстура"""
         assert smartphones_category.name == "Смартфоны"
-        assert "Samsung Galaxy S23 Ultra" in smartphones_category.products
+        assert len(smartphones_category.products.split("\n")) == 3
